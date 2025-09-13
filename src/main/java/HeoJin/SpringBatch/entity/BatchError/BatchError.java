f667e16cfd68d1496f67972cba3aeaf68fbfa279
Jin <hurjin1109@naver.com>
2025-09-13 14:40:33 +0900

    FEAT : 커스텀 배치 에러 테이블 추가

---
+++ src/main/java/HeoJin/SpringBatch/entity/BatchError/BatchError.java
@@ -0,0 +1,23 @@
+package HeoJin.SpringBatch.entity.BatchError;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatchError {
+    // mysql 에 저장
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sourceUrl;
+    private String siteIndex;
+}
